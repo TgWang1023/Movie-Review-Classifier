259bc80aa5ac316ea16fb47d56ff6ba3b98c9f41
Tiange Wang <wtgkevin@gmail.com>
2019-02-09 18:04:01 -0800

    reading and calculating from positive data

--- NaiveBayesClassifer.py
+++ NaiveBayesClassifer.py
@@ -1,5 +1,6 @@
 import sys
 import copy
+import math
 
 # Multinomial Naive Bayes Classifier
 # reading from positive training data
@@ -47,6 +48,59 @@ for word, count in neg_dict.items():
     else:
         total_dict[word] = count
 
-print(len(pos_dict))
-print(len(neg_dict))
-print(len(total_dict))
+# recording accuracy
+total_guesses = 0
+correct_guesses = 0
+
+# reading from positive test data
+f3 = open(sys.argv[3], 'r')
+review = []
+pos_result = 1.0
+neg_result = 1.0
+for w in f3.read().split():
+    # print(w)
+    if w != "/><br":
+        if w[-3:] == "<br":
+            w = w[:-3]
+        if w[:2] == "/>":
+            w = w[2:]
+        review.append(w)
+    else:
+        for word in review:
+            if word in pos_dict:
+                new_pos = pos_result * (pos_dict[word] / total_pos_words)
+                if new_pos <= 0:
+                    pos_result = math.log10(pos_result)
+                    new_pos = pos_result * (pos_dict[word] / total_pos_words)
+                pos_result = new_pos
+            else:
+                # laplace smoothing
+                new_pos = pos_result * (1 / total_pos_words)
+                if new_pos <= 0:
+                    pos_result = math.log10(pos_result)
+                    new_pos = pos_result * (pos_dict[word] / total_pos_words)
+                pos_result = new_pos
+            if word in neg_dict:
+                new_neg = neg_result * (neg_dict[word] / total_neg_words)
+                if new_neg <= 0:
+                    neg_result = math.log10(neg_result)
+                    new_neg = neg_result * (neg_dict[word] / total_neg_words)
+                neg_result = new_neg
+            else:
+                # laplace smoothing
+                new_neg = neg_result * (1 / total_neg_words)
+                if new_neg <= 0:
+                    neg_result = math.log10(neg_result)
+                    new_neg = neg_result * (neg_dict[word] / total_neg_words)
+                neg_result = new_neg
+        print("Positive Probability: " + pos_result + ", Negative Probability: " + neg_result)
+        if pos_result >= neg_result:
+            print("Predicted: Positive / Actual: Positive")
+            correct_guesses += 1
+        else:
+            print("Predicted: Negative / Actual: Positive")
+        total_guesses += 1
+        # reset parameters
+        review = 0
+        pos_result = 1.0
+        neg_result = 1.0
